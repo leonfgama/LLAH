60872778b26a8c3d2cf355d5aea4a4182978fa84
leonfgama <leonfgama@hotmail.com>
2016-02-01 16:54:52 -0200

    Lago aqui lago ai?

---
+++ LLAH.Web.Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LLAH.Web.Admin.MvcApplication" Language="C#" %>
---
+++ LLAH.Web.Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LLAH.Web.Admin.Startup))]
+namespace LLAH.Web.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
